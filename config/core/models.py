679f71a79ac28b005c64c573c4b22d66d1ac2cb2
kuzma <kuzma.dariusz@gmail.com>
2020-09-25 10:53:30 +0200

    add all basic models

--- config/core/models.py
+++ config/core/models.py
@@ -4,6 +4,11 @@ STARS_CHOICES = [
     (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6)
 ]
 
+CATERING_OPTIONS = [
+    ('BB', 'bed & breakfast'), ('HB', 'half board'), ('FB', 'full board'), ('Al', 'all inclusive'), ('OV', 'overnight'),
+    ('SC', 'self catering'), ('PP', 'program package'), ('ZPR', 'ZPR')
+]
+
 
 class Continent(models.Model):
     name = models.CharField(max_length=50)
@@ -62,10 +67,24 @@ class Airport(models.Model):
 
 
 class Trip(models.Model):
-    #todo - create class
-    pass
+    departure_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    departure_airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    arrival_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    arrival_hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    arrival_airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    date_of_departure = models.DateTimeField()
+    date_of_arrival = models.DateTimeField()
+    date_of_return = models.DateTimeField()
+    days = models.DurationField()
+    catering_option = models.CharField(choices=CATERING_OPTIONS, max_length=20)
+    price_for_adult = models.DecimalField(decimal_places=2)
+    price_for_child = models.DecimalField(decimal_places=2)
+    promoting = models.BooleanField(default=False)
+    adults_number = models.IntegerField(default=0)
+    kids_number = models.IntegerField(default=0)
 
 
 class TripPurchase(models.Model):
-    #todo - create class
-    pass
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    participants_data = models.CharField(max_length=1000)
+    price = models.DecimalField(decimal_places=2)
