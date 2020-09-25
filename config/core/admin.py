81cd41c927c3e45f65f8ace8293683c7a58ec316
kuzma <kuzma.dariusz@gmail.com>
2020-09-25 10:56:59 +0200

    register models in admin

--- config/core/admin.py
+++ config/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Continent, Country, City, Airport, Hotel, Trip, TripPurchase
+
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Airport)
+admin.site.register(Hotel)
+admin.site.register(Trip)
+admin.site.register(TripPurchase)
