1e6295b3f0edff8f69882305605c700357604a8d
kuzma <kuzma.dariusz@gmail.com>
2020-09-29 11:02:38 +0200

    add PromotedTripsView

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import PromotedTripsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', PromotedTripsView.as_view(), name='promoted_trips'),
 ]
--- config/core/views.py
+++ config/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from core.models import Trip
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PromotedTripsView(ListView):
+    model = Trip
+    template_name = 'core/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['promoted_trips_list'] = Trip.objects.filter(promoting=True)
+        return context
+
+
