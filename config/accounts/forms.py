a12932a22c3029a511276389eb9dc7b01abb3a90
kuzma <kuzma.dariusz@gmail.com>
2020-10-09 10:24:30 +0200

    add accounts app

---
+++ config/accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+# class SubmittableForm(Form):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.helper = FormHelper()
+#         self.helper.layout = Layout(*self.fields, Submit('submit', 'Submit'))
+
+
+class SignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'first_name']
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        return user
